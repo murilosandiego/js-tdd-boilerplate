5508102d38e18a811b87638ccd71f768bd682142
Murilo Sandiego <murilosandiego1@gmail.com>
2019-02-04 22:55:55 -0200

    Fix code

--- src/main.js
+++ src/main.js
@@ -1,10 +1,10 @@
 const FizzBuzz = (num) => {
-  if (num === 0) return 0;
-  if (num % 3 === 0 && num % 5 === 0) return 'FizzBuzz';
-  if (num % 3 === 0) return 'Fizz';
-  if (num % 5 === 0) return 'Buzz';
+  if (num === 0) return 0
+  if (num % 3 === 0 && num % 5 === 0) return 'FizzBuzz'
+  if (num % 3 === 0) return 'Fizz'
+  if (num % 5 === 0) return 'Buzz'
 
-  return num;
-};
+  return num
+}
 
-export default FizzBuzz;
+export default FizzBuzz
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -9,26 +9,26 @@ Se o número for divisível por 5, no lugar do número escreva 'Buzz' - X
 Se o número for divisível por 3 e 5, no lugar do número escreva 'FizzBuzz' - X
 Se não for múltiplo de nada, retorna o número
  */
-import { expect } from 'chai';
-import FizzBuzz from '../src/main';
+import { expect } from 'chai'
+import FizzBuzz from '../src/main'
 
 describe('FizzBuzz', () => {
   it('should FizzBuzz lib exists', () => {
-    expect(FizzBuzz).to.exist;
-  });
+    expect(FizzBuzz).to.exist
+  })
   it('should return `Fizz` when multiple of 3', () => {
-    expect(FizzBuzz(3)).to.equal('Fizz');
-    expect(FizzBuzz(9)).to.equal('Fizz');
-  });
+    expect(FizzBuzz(3)).to.equal('Fizz')
+    expect(FizzBuzz(9)).to.equal('Fizz')
+  })
   it('should return `Buzz` when multiple of 5', () => {
-    expect(FizzBuzz(5)).to.equal('Buzz');
-    expect(FizzBuzz(10)).to.equal('Buzz');
-  });
+    expect(FizzBuzz(5)).to.equal('Buzz')
+    expect(FizzBuzz(10)).to.equal('Buzz')
+  })
   it('sould return `FizzBuzz` when multiple of 5 and 3 ', () => {
-    expect(FizzBuzz(15)).to.equal('FizzBuzz');
-  });
+    expect(FizzBuzz(15)).to.equal('FizzBuzz')
+  })
   it('should return number when non-multiple', () => {
-    expect(FizzBuzz(7)).to.equal(7);
-    expect(FizzBuzz(0)).to.equal(0);
-  });
-});
+    expect(FizzBuzz(7)).to.equal(7)
+    expect(FizzBuzz(0)).to.equal(0)
+  })
+})
